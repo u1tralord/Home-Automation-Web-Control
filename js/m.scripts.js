a6f356ff7768ff48a23c0ef83ac1064b0d32914f
u1tralord <poston.jacob@gmail.com>
2015-07-20 12:00:34 -0400

    Added files

---
+++ js/m.scripts.js
@@ -0,0 +1,35 @@
+window.addEventListener('orientationchange', calculateTileSize);
+calculateTileSize()
+
+var songArea = $("#songs");
+for(var i = 0; i < 2000; i++){
+	var songItem = $("<button class='song-item'></button>").text("Song #"+i);
+	songArea.append(songItem);
+}
+function calculateTileSize()
+{
+    var bodyWidth = $(".panel-body").first().width();
+    var bodyHeight = $(".panel-body").first().height();
+    var squareSize = 125;
+    switch(window.orientation) 
+    {  
+        case -90:
+        case 90:
+            squareSize = (bodyHeight/2);
+            break;
+        default:
+            squareSize = (bodyWidth/2);
+            break; 
+    }
+    $(".tile").each(function(){
+        $(this).width(squareSize);
+        $(this).height(squareSize);
+    });
+}
+
+$(".tab-nav").click(function() {
+  $( ".panel-screen" ).each(function() {
+    $( this ).removeClass( "active-panel" );
+  });
+  $("#"+$(this).attr('linkTo')).addClass("active-panel");
+});
